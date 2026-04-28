704269c760eca54afe86b24ea6ca661a2b82083d
Toan Phung <toan.phung@proteo.info>
2026-04-28 20:47:53 +0100

    update

--- presentations/2026-04-12/adoption.ts
+++ presentations/2026-04-12/adoption.ts
@@ -8,7 +8,6 @@ export const section = {
         <tbody>
             <tr><td>Ontology lookup is manual</td><td>Finding the right term or NT/AC pair is time consuming and requires external tools</td></tr>
             <tr><td>Column schema is complex</td><td>Easy to use the wrong category or miss required fields</td></tr>
-            <tr><td>No template reuse</td><td>Every new experiment starts from scratch</td></tr>
             <tr><td>No official requirements</td><td>PRIDE recommends user to submit SDRF with their raw data but does not make it a requirement</td></tr>
         </tbody>
     </table>
